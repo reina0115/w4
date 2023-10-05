70b8e495f5be9cf58b0e0033a5db3506902d4a4a
reina0115 <131437146+reina0115@users.noreply.github.com>
2023-10-05 10:45:12 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '外は４０度だったので :insertx: は歩いて行った. :inserty:　に着いたとき、:insertz:　:insertx:は驚いたが、体重１００kgの スライム　は驚かなかった. :insertx:ではよくあることだった。';
+const storyText = '外は４０度だったので :insertx: は歩いて行った. :inserty:　に着いたとき、:insertz:　あい　は驚いたが、体重１００kgの スライム　は驚かなかった. :inserty:ではよくあることだった。';
 const insertX = ['アーニャ', 'リムル', 'ルディ'];
 const insertY = ['ディズニーランド', 'ハウステンボス', '公園'];
 const insertZ = ['雪が降っていた', '地面が溶けていた', '混雑していた'];
@@ -28,7 +28,7 @@ function result() {
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob', name);
+    newStory = newStory.replace('あい', name);
   }
 
   if (document.getElementById("uk").checked) {
